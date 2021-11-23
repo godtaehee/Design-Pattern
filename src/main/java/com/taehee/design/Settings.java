e1c8b860645bc8fda054b1d1000b614219ff19c2
taehee <wonderfulhuman@naver.com>
2021-11-23 21:32:24 +0900

    Thread-Safe하게 싱글톤 구현하는 방법 1: synchronized 키워드 사용하기 Feat: 이렇게 synchronized 키워드를 통해 다수의 스레드가 getInstance의 함수에 접근하지 못하도록 함으로서 스레드 세이프를 가장 쉽게 보장할수 있다.
    하지만 이렇게 synchronized 키워드를 통해 동기화 하는 작업이 계속 들다 보니 성능에 이슈가 생길수 있다는 단점이 있다.
    
    동기화라는 자체가 Lock을 사용하게되는데 해당 Lock을 잡고있는 스레드만 이 영역에 접근하게끔 해주는 매커니즘이다.
    
    그러고나서 다 쓰고나면 Lock을 해제한다.

--- src/main/java/com/taehee/design/Settings.java
+++ src/main/java/com/taehee/design/Settings.java
@@ -5,7 +5,7 @@ public class Settings {
 
   private Settings() {}
 
-  public static Settings getInstance() {
+  public static synchronized Settings getInstance() {
     if (instance == null) {
       instance = new Settings();
     }
