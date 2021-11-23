65f28f691bc3809b32205bdc7debcac7874b258b
taehee <wonderfulhuman@naver.com>
2021-11-23 21:38:31 +0900

    Thread-Safe하게 싱글톤을 구현하는 방법 2: 이른 초기화(Eager Initialization) 사용하기
    객체를 생성하는 비용이 그렇게 크지 않다고 하면 이 방법을 사용해도 좋다.
    
    그냥 아예 객체를 미리 만들어 놓는 방법인데 이 미리 만든다라는게 오히려 단점이 될수도있다.
    
    만드는 과정이 길고 메모리를 많이 사용한다면 만들어는 놨는데 쓰질않는다고하면 애플리케이션을 실행할때 기껏 리소스를 많이써서 많들어놨더니
    안쓰는꼴이된다.

--- src/main/java/com/taehee/design/Settings.java
+++ src/main/java/com/taehee/design/Settings.java
@@ -1,14 +1,11 @@
 package com.taehee.design;
 
 public class Settings {
-  private static Settings instance;
+  private static final Settings INSTANCE = new Settings();
 
   private Settings() {}
 
-  public static synchronized Settings getInstance() {
-    if (instance == null) {
-      instance = new Settings();
-    }
-    return instance;
+  public static Settings getInstance() {
+    return INSTANCE;
   }
 }
