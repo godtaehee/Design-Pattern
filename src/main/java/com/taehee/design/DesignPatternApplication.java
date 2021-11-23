b57652392464de87f41b8000540ed6c78ed2f17f
taehee <wonderfulhuman@naver.com>
2021-11-23 22:28:07 +0900

    싱글톤을 깨뜨리는 방법 1: 리플렉션 사용하기

--- src/main/java/com/taehee/design/DesignPatternApplication.java
+++ src/main/java/com/taehee/design/DesignPatternApplication.java
@@ -3,17 +3,21 @@ package com.taehee.design;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 @SpringBootApplication
 public class DesignPatternApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
-    Settings settings1 = Settings.getInstance();
-    Settings settings2 = Settings.getInstance();
+    Settings settings = Settings.getInstance();
 
-    System.out.println(settings1 == settings2);
-    System.out.println(settings1 == Settings.getInstance());
-    System.out.println(settings2 == Settings.getInstance());
+    Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+    constructor.setAccessible(true);
+    Settings reflectionInstance = constructor.newInstance();
+
+    System.out.println(settings == reflectionInstance);
 
   }
 }
