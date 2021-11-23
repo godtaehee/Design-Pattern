0f5702e0871c02072d0a5b10cd8ba739e23e9922
taehee <wonderfulhuman@naver.com>
2021-11-23 23:19:43 +0900

    enum을 사용하게되면 역직렬화와 직렬화에도 안전하다.
    하지만 enum은 상속을 못한다.
    
    한다 하더라도 enum을 상속해야한다.

--- src/main/java/com/taehee/design/DesignPatternApplication.java
+++ src/main/java/com/taehee/design/DesignPatternApplication.java
@@ -10,17 +10,17 @@ import java.lang.reflect.InvocationTargetException;
 @SpringBootApplication
 public class DesignPatternApplication {
 
-  public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
 
     Settings settings = Settings.INSTANCE;
-
     Settings settings1 = null;
 
-    Constructor<?>[] declaredConstructors = Settings.class.getDeclaredConstructors();
-    for (Constructor<?> constructor : declaredConstructors) {
-      constructor.setAccessible(true);
-      settings1 = (Settings) constructor.newInstance("INSTANCE");
+    try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+      out.writeObject(settings);
+    }
 
+    try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+      settings1 = (Settings) in.readObject();
     }
 
     System.out.println(settings == settings1);
